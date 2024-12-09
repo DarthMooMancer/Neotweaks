ee0337751a677e770c8d7145b8400857f74b5334
DarthMooMancer <haynes.a763@outlook.com>
2024-12-09 08:12:45 -0500

    adding files

---
+++ plugins/settings.lua
@@ -0,0 +1,12 @@
+vim.opt.termguicolors = true
+vim.opt.completeopt = { "menuone", "noselect", "noinsert", "popup" }
+vim.opt.mouse = ""
+vim.opt.number = true
+vim.opt.wrap = false
+vim.opt.expandtab = true
+vim.opt.relativenumber = true
+vim.opt.syntax = "enable"
+vim.opt.scrolloff = 8
+vim.opt.swapfile = false
+vim.opt.hlsearch = false
+
